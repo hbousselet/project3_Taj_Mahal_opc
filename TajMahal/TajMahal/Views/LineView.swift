df5b67bb2110f1a78d3a30e441fa3eea4a162e34
Hugues Bousselet <hugues.bousselet@gmail.com>
2024-10-10 17:27:09 +0200

    First commit, creation of the base of the WelcomeView. Need to fix subviews size and position

---
+++ TajMahal/TajMahal/Views/LineView.swift
@@ -0,0 +1,42 @@
+//
+//  LineView.swift
+//  TajMahal
+//
+//  Created by Hugues Bousselet on 10/10/2024.
+//
+
+import SwiftUI
+
+struct LineView: View {
+    let imageName: String
+    let leftText: String
+    let rightText: String
+    
+    var body: some View {
+        HStack(alignment: .firstTextBaseline) {
+            Image(systemName: imageName).frame(width: 12, height: 12)
+                .padding(.leading)
+            Text(leftText)
+                .padding(.leading, 10)
+            Spacer()
+            Text(rightText)
+        }
+        .frame(width: 335, height: 15)
+        .foregroundStyle((Color(.sRGB, red: 102/255, green: 102/255, blue: 102/255, opacity: 1)))
+        .font(Font.custom("PlusJakartaSans-Regular", size: 12))
+        .fontWeight(.bold)
+        .multilineTextAlignment(.trailing)
+    }
+}
+
+#Preview {
+    LineView(imageName: "clock", leftText: "monday", rightText: "blbla")
+}
+
+
+//font-family: Plus Jakarta Sans;
+//font-size: 12px;
+//font-weight: 600;
+//line-height: 15.12px;
+//letter-spacing: -0.16500000655651093px;
+//text-align: right;
--- TajMahal/TajMahal/Views/WelcomeView.swift
+++ TajMahal/TajMahal/Views/WelcomeView.swift
@@ -10,18 +10,76 @@ import SwiftUI
 // Page d'accueil
 struct WelcomeView: View {
     var body: some View {
+        
         NavigationStack {
             Image("TajMahal")
+                .resizable()
+                .frame(width: 335, height: 423)
+                .padding(.top, 51)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Restaurant Indien")
+                        .font(Font.custom("PlusJakartaSans-Regular", size: 12))
+                        .fontWeight(.heavy)
+                        .foregroundStyle((Color(.sRGB, red: 102/255, green: 102/255, blue: 102/255, opacity: 1)))
+                    Spacer()
+                    Text("Taj Mahal")
+                        .font(Font.custom("PlusJakartaSans-Regular", size: 18))
+                        .fontWeight(.black)
+                        .foregroundStyle((Color(.sRGB, red: 51/255, green: 51/255, blue: 51/255, opacity: 1)))
+
+                }
+                .foregroundStyle((Color(.sRGB, red: 154/255, green: 154/255, blue: 154/255, opacity: 1)))
+                Spacer()
+                Image("Logo")
+                    .resizable()
+                    .background(Color(.sRGB, red: 154/255, green: 154/255, blue: 154/255, opacity: 1))
+                    .frame(width: 39.99, height: 40)
+            }
+            .frame(width: 335, height: 40)
+            .padding(.top, 32)
+            VStack(alignment: .leading) {
+                LineView(imageName: "clock", leftText: "Mardi", rightText: "11h30 - 14h30・ 18h30 - 22h00")
+                LineView(imageName: "frying.pan", leftText: "Type de Service", rightText: "À emporter")
+                LineView(imageName: "location", leftText: "12  Avenue de la Brique - 75010 Paris", rightText: "")
+                LineView(imageName: "figure", leftText: "www.tajmahal.fr", rightText: "")
+                LineView(imageName: "phone", leftText: "06 12 34 56 78", rightText: "")
+            }
+            .frame(width: 337, height: 130, alignment: .leading)
+            .padding(.top, 22)
+            .lineSpacing(15.12)
+
             Spacer()
             NavigationLink {
                 MenuView()
             } label : {
-                Text("Menu")
+                Text("Accéder au menu")
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(
+                        RoundedRectangle(cornerRadius: 10)
+                        .fill(Color(.sRGB, red: 207/255, green: 47/255, blue: 47/255, opacity: 1))
+                )
+                    .font(Font.custom("PlusJakartaSans-Regular", size: 16))
+                    .fontWeight(.heavy)
+                    .foregroundStyle(.white)
             }
+            .frame(width: 335, height: 40)
         }
+
     }
 }
 
 #Preview {
     WelcomeView()
 }
+
+//width: 335px;
+//height: 40px;
+//top: 740px;
+//left: 20px;
+//gap: 0px;
+//border-radius: 10px 0px 0px 0px;
+//opacity: 0px;
+
+
+
